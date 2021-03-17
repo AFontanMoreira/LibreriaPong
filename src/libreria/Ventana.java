e1d34613ba83ad611ad08c24f056195a5a7f9ae5
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-03-17 13:34:24 +0100

    Initial commit

---
+++ src/libreria/Ventana.java
@@ -0,0 +1,19 @@
+package libreria;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+
+public class Ventana extends JFrame {
+
+    public Ventana(String nombre, int ANCHO, int ALTO, JPanel lamina, Thread hilo, KeyAdapter teclado) {
+        setTitle(nombre);
+        setSize(ANCHO,ALTO);
+        setLocationRelativeTo(null); //Ventana en el centro de la pantalla
+        setResizable(false);
+        add(lamina);
+        addKeyListener(teclado);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        hilo.start();
+    }
+}
